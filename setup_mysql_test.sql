70c0c245c6d6038bdfea19dfcedbf29a064991eb
Allan <allanshanza@gmail.com>
2023-08-22 06:53:08 -0700

    initial commit

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,19 @@
 -- prepares a MySQL server 
 -- for testing of the AirBnB clone project
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+-- Grant privileges to the user on the database
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges to apply the changes
+FLUSH PRIVILEGES;
