f88037e2d2b854b41cb725d4a1995c089783c9b7
Allan <allanshanza@gmail.com>
2023-08-22 07:07:52 -0700

    Updated File Storage in engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,3 +48,10 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Deletes Obj from __objects if it's inside"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.all():
+                del self.all()[key]
