8f4f15941a905b3a080cd95cfe720f696b585e88
Allan <allanshanza@gmail.com>
2023-08-22 07:26:30 -0700

    Updated Place: in models/place.py

--- models/place.py
+++ models/place.py
@@ -1,18 +1,33 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = 'places'
+
+    reviews = relationship("Review", back_populates="place", cascade="all, delete-orphan")
+
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
+
+    @property
+    def reviews(self):
+        """Getter attribute for reviews (FileStorage relationship)"""
+        from models import storage
+        from models.review import Review
+
+        all_reviews = storage.all(Review)
+        return [review for review in all_reviews.values() if review.place_id == self.id]
