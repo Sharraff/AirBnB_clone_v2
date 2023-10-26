07831726749f67efc70b616be196dc335e748ec6
Sharraff <rogandsteinporter@gmail.com>
2023-10-26 02:22:56 -0700

    display cities and names

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
-"""starts a Flask web application"""
-
+# -*- coding: utf-8 -*-
+"""
+@author: Musharraff Ibrahim
+"""
+from models import storage
+from models.state import State
 from flask import Flask, render_template
-import models
-
-app = Flask("__name__")
+app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def refresh(exception):
-        models.storage.close()
+def appcontext_teardown(self):
+    """use storage for fetching data from the storage engine
+    """
+    storage.close()
 
 
-
-@app.route("/states_list", strict_slashes=False)
-def route_states():
-        pep_fix = models.dummy_classes["State"]
-        data = models.storage.all(cls=pep_fix)
-        states = data.values()
-        return render_template('7-states_list.html', states_list=states)
+@app.route('/cities_by_states', strict_slashes=False)
+def state_info():
+    """Display a HTML page inside the tag BODY"""
+    return render_template('8-cities_by_states.html',
+                           states=storage.all(State))
 
 
-
-@app.route("/cities_by_states", strict_slashes=False)
-def route_city():
-        pep_fix = models.dummy_classes["State"]
-        data = models.storage.all(cls=pep_fix)
-        states = data.values()
-        return render_template('8-cities_by_states.html', states_list=states)
-
-
-if __name__ == "__main__":
-        app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
